f7a1b79bfab5087c80cb1e9df2447790780a2c7e
Shiva <itsmegogo@gmail.com>
2014-01-10 20:36:20 +0530

    Create README.md

---
+++ cms_check_hsphere.sh
@@ -0,0 +1,29 @@
+# Build Domain list from Apache Config
+domain_list(){
+}
+
+# Check New Domains
+new_domain(){
+
+}
+
+# Check Domain IP
+domain_ip(){
+
+}
+
+# Build IP List
+server_IP_list(){
+	
+}
+
+# Check CMS for New Domain
+# WordPress, Joomla, PHPBB
+check_cms(){
+
+}
+
+# Send Mail
+send_mail(){
+
+}
